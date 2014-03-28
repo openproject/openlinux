c2bcfc58a69f0b54b8acf3d227cbb91107720ae7
Jay <fjtianxia@gmail.com>
2014-03-28 22:28:11 +0800

    增加配置JAVA1.6(android需要1.6就行了)，后期考虑增加1.7, 1.8版本的JAVA

--- Install.sh
+++ Install.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-a= 'hjell'
-echo "$a"
-# chmod 0755 -R ~/.openlinux/tools/
+. ~/.bashrc
+chmod 0755 -R ~/.openlinux/tools/
 
-# . ~/.bashrc
-
-open = "bin"
+LINUX_OS_VERSION=$(uname -m)
+OPEN_LINUX_BIN="bin"
 if [ ! -d $OPEN_LINUX_BIN ]; then
-    echo $OPEN_LINUX_BIN
     mkdir $OPEN_LINUX_BIN
 fi
 
-# export $JAVA_BIN = "bin/jdk-6u45-linux-i586.bin"
-
-# if [ ! -f "$JAVA_BIN" ]; then
-    # curl http://kaiyuanxiangmu-openproject.stor.sinaapp.com/jdk-6u45-linux-i586.bin > "$JAVA_BIN"
-    # chmod a+x "$JAVA_BIN"
-    # . "$JAVA_BIN"
-# fi
-
+# JAVA 配置(1.6)
+if [ $LINUX_OS_VERSION=='x86_64' ]; then
+    JAVA_BIN="jdk-6u45-linux-x64.bin"
+else
+    JAVA_BIN="jdk-6u45-linux-i586.bin"
+fi
+if [ ! -f "$OPEN_LINUX_BIN/$JAVA_BIN" ]; then
+    curl http://kaiyuanxiangmu-openproject.stor.sinaapp.com/$JAVA_BIN > "$OPEN_LINUX_BIN/$JAVA_BIN"
+fi
+cd $OPEN_LINUX_BIN
+chmod a+x "$JAVA_BIN"
+./$JAVA_BIN
+cd ..
